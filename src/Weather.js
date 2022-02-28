b27e870c51cadaa3912f68bae06785946abecd07
Emi Dumigan <emidumigan@gmail.com>
2022-02-28 13:52:33 +1100

    api link fixed

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ export default function Weather(props) {
 }
 
 let apiKey = "5596812fa6646bbfaf224a9f62eed9c6";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(handleResponse);
     return <h2>Hello from Weather</h2>;
