fee0e8bd407afe054b2fcee38ddebccba41a6da8
Emi Dumigan <emidumigan@gmail.com>
2022-03-29 17:33:09 +0930

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Tokyo"/>
-      </header>
+      Weather App
     </div>
   );
 }
