0dbae27c8053886b007c4553da53946ff9519689
Emi Dumigan <emidumigan@gmail.com>
2022-04-12 12:34:02 +0930

    Api updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,89 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-    let weatherData = {
-      city: "Melbourne",
-      date: "Friday 17:16",
-      condition: "Clear Sky",
-      imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-      temperature: "22",
-      wind: "2",
-      humidity: "90",
-      minTemp: "16",
-      maxTemp: "23"
-    };
+
   
-    return (
-      
-      <div classNAme="Weather">
-        
-       
+  const [weatherData, setWeatherData] = useState({ready: false});
+
+  function handleResponse(response){
+    console.log(response.data);
+    setWeatherData({
+    ready: true,
+    city: response.data.name,
+    date: "Tuesday 13:10",
+    temperature: response.data.main.temp,
+    wind: response.data.wind.speed,
+    humidity: response.data.main.humidity,
+    description: response.data.weather[0].description,
+    iconUrl: "http://openweathermap.org/img/wn/02n@2x.png"
+   
+  });
+
+  }
+
+  if(weatherData.ready){ 
+    return
+( 
+<div className="Weather">
         <div className="wrapper">
-          <form classNAme="search-form nb-3">
-            <div classNAme="row">
-              <div classNAme="col-9">
+          <form className="search-form nb-3">
+            <div className="row">
+              <div className="col-9">
+              
                 <input
                   type="search"
-                  placeholder="Enter a city"
+                  placeholder="Enter a city..."
                   autoFocus="on"
                   autocomplete="off"
-                  classNAme="form-control form"
+                  className="form-control form"
                 />
               </div>
-              <div classNAme="col-3">
+              <div className="col-3">
                 <input
                   type="submit"
                   value="Search"
-                  classNAme="form-control btn btn-primary w-100"
+                  className="form-control btn btn-primary w-100"
                 />
               </div>
             </div>
           </form>
   
-          <div classNAme="overview">
+          <div className="overview">
             <h1>{weatherData.city}</h1>
             <ul>
-              <li classNAme="date">{weatherData.date}</li>
-              <li classNAme="weather-condition">{weatherData.condition}</li>
+              <li className="date">{weatherData.date}</li>
+              <li className="weather-condition text-capitalize">{weatherData.description}</li>
             </ul>
           </div>
   
           <div className="clearfix weather-temperature">
             <img
-              src={weatherData.imgUrl}
-              alt={weatherData.condition}
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
               className="float-letf"
             />
             <span className="float-right ">
-              <strong>{weatherData.temperature}</strong>
+              <strong>{weatherData.Temperature}</strong>
               <span className="units">°C</span>
             </span>
           </div>
   
-          <div className="weather-descriptions" id="description">
+          <div className="weather-descriptions">
             <div className="row justify-content-center">
-              <div classNAme="col-md-4 weather-descriptions-box">
-                <div classNAme="desctiption-title">Wind</div>
-                <div classNAme="description">
+              <div className="col-md-4 weather-descriptions-box">
+                <div className="desctiption-title">Wind</div>
+                <div className="description">
                   <span>{weatherData.wind}</span>km/h
                 </div>
               </div>
   
-              <div classNAme="col-md-4 weather-descriptions-box">
-                <div classNAme="desctiption-title">Humidity</div>
-                <div classNAme="description">
+              <div className="col-md-4 weather-descriptions-box">
+                <div className="desctiption-title">Humidity</div>
+                <div className="description">
                   <span>{weatherData.humidity}</span>%
                 </div>
               </div>
   
-              <div classNAme="col-md-4 weather-descriptions-box">
-                <div classNAme="desctiption-title">Temperature</div>
-                <div classNAme="description">
+              <div className="col-md-4 weather-descriptions-box">
+                <div className="desctiption-title">Temperature</div>
+                <div className="description">
                   <strong>
-                    <span>{weatherData.maxTemp}</span>°
+                    <span>{weatherData.temperature}</span>°
                   </strong>{" "}
-                  | <span>{weatherData.minTemp}</span>°
+                  | <span></span>°
                 </div>
               </div>
             </div>
@@ -93,5 +102,17 @@ export default function Weather() {
         </div>
        
       </div>
-    );
-  }
+      );
+  } else {
+  const apiKey = "5596812fa6646bbfaf224a9f62eed9c6";
+  let city = "New Yorl";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse); 
+
+  return "Loading..."
+  
+};
+}
+ 
+
+  
