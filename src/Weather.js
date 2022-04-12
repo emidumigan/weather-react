85955b21dcc8172eb6c4826a09f208f95e7aaa42
Emi Dumigan <emidumigan@gmail.com>
2022-04-12 12:38:26 +0930

    js updated

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
 
   
   const [weatherData, setWeatherData] = useState({ready: false});
@@ -105,8 +105,7 @@ export default function Weather() {
       );
   } else {
   const apiKey = "5596812fa6646bbfaf224a9f62eed9c6";
-  let city = "New Yorl";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse); 
 
   return "Loading..."
